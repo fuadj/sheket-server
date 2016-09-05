870edbfab5e7f640e9a5777a32c75330559f7f41
Fuad Seffa <fuadjemal@gmail.com>
2016-09-05 22:11:18 +0300

    Refactor signing logic.

--- controller/signature/signer.go
+++ controller/signature/signer.go
@@ -15,133 +15,44 @@ import (
 /*
  * Sign-es the message and encodes it in Base64 format.
  */
-func SignEncodeBase64Message(msg string) (string, error) {
-	signed, err := signer.Sign([]byte(msg))
+func SignBase64EncodeMessage(msg string) (string, error) {
+	h := sha256.New()
+	h.Write([]byte(msg))
+	d := h.Sum(nil)
+	signed, err := rsa.SignPKCS1v15(nil, private_key, crypto.SHA256, d)
 	if err != nil {
 		return "", err
 	}
 	return base64.StdEncoding.EncodeToString(signed), nil
 }
 
-var signer _Signer
+var private_key *rsa.PrivateKey
 
 func init() {
 	var err error
-	if signer, err = loadPrivateKey("./private_key.pem"); err != nil {
+	if private_key, err = loadPrivateKey("./private_key.pem"); err != nil {
 		panic(fmt.Errorf("Can't load private key '%s'", err.Error()))
 	}
 }
 
-func loadPublicKey(path string) (_Unsigner, error) {
-	return parsePublicKey([]byte(`-----BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDCFENGw33yGihy92pDjZQhl0C3
-6rPJj+CvfSC8+q28hxA161QFNUd13wuCTUcq0Qd2qsBe/2hFyc2DCJJg0h1L78+6
-Z4UMR7EOcpfdUE9Hf3m/hs+FUR45uBJeDK1HSFHD8bHKD6kv8FPGfJTotc+2xjJw
-oYi+1hqp1fIekaxsyQIDAQAB
------END PUBLIC KEY-----`))
-}
-
-func parsePublicKey(pemBytes []byte) (_Unsigner, error) {
-	block, _ := pem.Decode(pemBytes)
-	if block == nil {
-		return nil, errors.New("ssh: no key found")
-	}
-
-	var rawkey interface{}
-	switch block.Type {
-	case "PUBLIC KEY":
-		rsa, err := x509.ParsePKIXPublicKey(block.Bytes)
-		if err != nil {
-			return nil, err
-		}
-		rawkey = rsa
-	default:
-		return nil, fmt.Errorf("ssh: unsupported key type %q", block.Type)
-	}
-
-	return newUnsignerFromKey(rawkey)
-}
-
-func loadPrivateKey(key_path string) (_Signer, error) {
+func loadPrivateKey(key_path string) (*rsa.PrivateKey, error) {
 	contents, err := ioutil.ReadFile(key_path)
 	if err != nil {
 		return nil, err
 	}
-	return parsePrivateKey(contents)
-}
-
-func parsePrivateKey(pemBytes []byte) (_Signer, error) {
-	block, _ := pem.Decode(pemBytes)
+	block, _ := pem.Decode(contents)
 	if block == nil {
-		return nil, errors.New("ssh: no key found")
+		return nil, errors.New("can't decode key, no key found")
 	}
 
-	var rawkey interface{}
 	switch block.Type {
 	case "RSA PRIVATE KEY":
-		fmt.Printf("Prasing pck\n")
 		rsa, err := x509.ParsePKCS1PrivateKey(block.Bytes)
 		if err != nil {
-			fmt.Printf("Prasing pck error\n")
 			return nil, err
 		}
-		rawkey = rsa
+		return rsa, nil
 	default:
-		return nil, fmt.Errorf("ssh: unsupported key type %q", block.Type)
+		return nil, fmt.Errorf("unsupported key type %q", block.Type)
 	}
-	return newSignerFromKey(rawkey)
-}
-
-type _Signer interface {
-	Sign(data []byte) ([]byte, error)
-}
-
-type _Unsigner interface {
-	Unsign(data []byte, sig []byte) error
-}
-
-func newSignerFromKey(k interface{}) (_Signer, error) {
-	var sshKey _Signer
-	switch t := k.(type) {
-	case *rsa.PrivateKey:
-		sshKey = &rsaPrivateKey{t}
-	default:
-		return nil, fmt.Errorf("ssh: unsupported key type %T", k)
-	}
-	return sshKey, nil
-}
-
-func newUnsignerFromKey(k interface{}) (_Unsigner, error) {
-	var sshKey _Unsigner
-	switch t := k.(type) {
-	case *rsa.PublicKey:
-		sshKey = &rsaPublicKey{t}
-	default:
-		return nil, fmt.Errorf("ssh: unsupported key type %T", k)
-	}
-	return sshKey, nil
-}
-
-type rsaPublicKey struct {
-	*rsa.PublicKey
-}
-
-type rsaPrivateKey struct {
-	*rsa.PrivateKey
-}
-
-// Sign signs data with rsa-sha256
-func (r *rsaPrivateKey) Sign(data []byte) ([]byte, error) {
-	h := sha256.New()
-	h.Write(data)
-	d := h.Sum(nil)
-	return rsa.SignPKCS1v15(nil, r.PrivateKey, crypto.SHA256, d)
-}
-
-// Unsign verifies the message using a rsa-sha256 signature
-func (r *rsaPublicKey) Unsign(message []byte, sig []byte) error {
-	h := sha256.New()
-	h.Write(message)
-	d := h.Sum(nil)
-	return rsa.VerifyPKCS1v15(r.PublicKey, crypto.SHA256, d, sig)
 }
