ff58214280f6a207153b19f76ed84881dd6c24d4
Fuad Seffa <fuadjemal@gmail.com>
2016-10-16 22:59:30 +0300

    Refactor auth logic to gRPC. Remove un-necessary calls.

--- controller/auth/auth.go
+++ controller/auth/auth.go
@@ -1,14 +1,14 @@
 package auth
 
 import (
-	"fmt"
 	"github.com/gorilla/securecookie"
-	"net/http"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"sheket/server/models"
 )
 
 const (
-	login_cookie    = "user_log_in"
+	name_login_cookie = "user_log_in"
 	key_user_id     = "user_id"
 	invalid_user_id = -1
 )
@@ -21,37 +21,17 @@ var (
 		securecookie.GenerateRandomKey(32))
 )
 
-func GetCurrentUser(r *http.Request) (*models.User, error) {
-	if user_id, err := GetCurrentUserId(r); err == nil {
-		return Store.FindUserById(user_id)
-	}
-	return nil, fmt.Errorf("Can't find User")
-}
-
-func GetCurrentUserId(r *http.Request) (int64, error) {
-	if cookie, err := r.Cookie(login_cookie); err == nil {
-		decoded := make(map[string]int64)
-		if err = cookieHandler.Decode(login_cookie, cookie.Value, &decoded); err == nil {
-			return decoded[key_user_id], nil
-		} else {
-			return invalid_user_id, err
-		}
-	} else {
-		return invalid_user_id, fmt.Errorf("invalid login cookie")
-	}
-}
-
 func GetUser(login_cookie string) (*models.User, error) {
 	if user_id, err := GetUserId(login_cookie); err == nil {
 		return Store.FindUserById(user_id)
 	} else {
-		return nil, err
+		return nil, grpc.Errorf(codes.Unauthenticated, "Invalid login")
 	}
 }
 
-func GetUserId(cookie string) (int64, error) {
-	decoded := make(map[string]int64)
-	if err := cookieHandler.Decode(login_cookie, cookie, &decoded); err == nil {
+func GetUserId(cookie string) (int, error) {
+	decoded := make(map[string]int)
+	if err := cookieHandler.Decode(name_login_cookie, cookie, &decoded); err == nil {
 		return decoded[key_user_id], nil
 	} else {
 		return invalid_user_id, err
@@ -63,8 +43,8 @@ func GenerateLoginCookie(u *models.User) (string, error) {
 	var err error
 
 	if cookie, err = cookieHandler.Encode(
-		login_cookie,
-		map[string]int64{
+		name_login_cookie,
+		map[string]int{
 			key_user_id: u.UserId,
 		}); err == nil {
 		return cookie, nil
